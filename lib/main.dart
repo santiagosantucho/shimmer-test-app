7bdface5857951ca15f53ceb7b53f43db0029b0d
santiagosantucho <bitbucket2020>
2020-08-28 15:52:11 -0300

    Added shimmer dependency.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,15 +10,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shimmer Test App'),
+        title: 'Flutter Demo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Shimmer Test App'),
+          ),
+          body: Shimmer.fromColors(
+            baseColor: Colors.red,
+            highlightColor: Colors.blue,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('This is a text widget in the body'),
+                  SizedBox(height: 5),
+                  Container(
+                    width: 100,
+                    height: 50,
+                    color: Colors.red,
+                  ),
+                  SizedBox(height: 5),
+                  Container(
+                    width: 200,
+                    height: 50,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
-        body: Center(
-          child: Text('This is the body'),
-        ),
-      ),
     );
   }
 }
